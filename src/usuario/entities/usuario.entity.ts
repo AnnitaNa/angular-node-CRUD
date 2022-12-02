ffafb8f979832c7ef241222a59f8c5208f9d861b
Tatiane A. Nicchetti <tanicchetti@gmail.com>
2022-12-02 12:47:06 -0300

    create usuario entity

--- src/usuario/entities/usuario.entity.ts
+++ src/usuario/entities/usuario.entity.ts
@@ -1,1 +1,24 @@
-export class Usuario {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
+export class Usuario {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 120 })
+  name: string;
+
+  @Column({ length: 120 })
+  cpf: string;
+
+  @Column({ length: 120 })
+  email: string;
+
+  @Column({ length: 120 })
+  phone: string;
+
+  @Column({ length: 120 })
+  gender: string;
+
+  @Column({ type: 'date' })
+  birthday: string;
+}
