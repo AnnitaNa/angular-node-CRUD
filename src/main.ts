6c055478dec77dee6b9a21097652753bfb2bfae3
Tatiane A. Nicchetti <tanicchetti@gmail.com>
2022-12-05 13:05:03 -0300

    UPDATE

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(port, () => { console.log(`running on ${port}`) });
 }
 bootstrap();
