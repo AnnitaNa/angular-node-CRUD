d1478dfdc38dc23cd05d52642d0c4a5222888064
Tatiane A. Nicchetti <tanicchetti@gmail.com>
2022-12-05 13:03:16 -0300

    add environment url

--- src/app/services/usuarioProxy.service.ts
+++ src/app/services/usuarioProxy.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { IUsuario } from '../models/usuario.model';
 import { IUsuarioInput } from '../models/usuarioInput.model';
 
-const baseUrl: string = 'http://localhost:3000/usuario';
+const API_URL: string = `${environment.API_URL}`;
+const baseUrl: string = `${API_URL}/usuario`;
 
 @Injectable({
   providedIn: 'root',
